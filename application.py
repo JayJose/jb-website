50a22b5391055632374f6793384c284eea5904f3
John Murray <johnmurray@fastmail.com>
2021-09-20 20:05:31 -0400

    adds silly pic of slim jim

--- application.py
+++ application.py
@@ -1,19 +1,73 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import dcc, html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 
-external_stylesheets = [dbc.themes.DARKLY]
-dash_app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = dash_app.server
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-dash_app.layout = dbc.Container(
-    children=[
-        dbc.Row([
-            html.H2('UAB Payroll Explorer')
-        ]),
-    ]
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("John Murray", className="display-4"),
+        html.Hr(),
+        html.P(
+            "A simple sidebar layout with navigation links", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Page 1", href="/page-1", active="exact"),
+                dbc.NavLink("Page 2", href="/page-2", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
 )
 
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+pic = html.Img(id='slim', src='https://i.imgur.com/wyWuUXE.jpg', alt='Milo w/ something in his teef.')
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content, pic])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return html.P("This is the content of the home page!")
+    elif pathname == "/page-1":
+        return html.P("This is the content of page 1. Yay!")
+    elif pathname == "/page-2":
+        return html.P("Oh cool, this is page 2!")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
 if __name__ == '__main__':
-    dash_app.run_server(debug=True)
+    app.run_server(debug=True)
